28f0f6258e6cb1149fe8b11f70f367d002603d79
James Earle <jaearle@microsoft.com>
2017-09-21 13:04:27 -0700

    adding bootstrap homepage

--- app.js
+++ app.js
@@ -1,44 +1,40 @@
-﻿var body = require('body-parser');
 var express = require('express');
-var http = require('http');
 var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-// Allow requests to parse request body info
-app.use(express.bodyParser());
-
-// Make necessary parameters visible to 
-app.use(function (req, res, next) {
-    next();
-});
-
-// all environments
-app.set('port', process.env.PORT || 443);
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-}
-
-app.get('/', (req, res) => {
-    res.render('index');
+app.use('/', function (req, res) {
+	res.render('index', {title: "MyAppwww"});
 });
 
-app.post('/', (req, res) => {
-
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
-http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+// error handler
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
+
+module.exports = app;
